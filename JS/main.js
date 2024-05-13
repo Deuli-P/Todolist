077399678cf7d38e6abcce71d33c8ba08bcdafb1
Antoniutti Pierre <pierre.antoniutti@gmail.com>
2024-05-13 16:44:40 +0200

    Petite exercice JS vanilla jour 1 semaine 2 3WA

---
+++ JS/main.js
@@ -0,0 +1,136 @@
+
+let tasks = [
+    {
+        text:"Apprendre le Javascript",
+        priority: 1,
+    },
+    {
+        text:"Créer un compte Github",
+        priority: 2,
+    },
+    {
+        text:"Repondre à mes emails",
+        priority: 3,
+    },
+    {
+        text:"Veille Informatique",
+        priority: 2,
+    },
+]
+
+
+const inputTitle = document.querySelector("#input-title");
+const inputPriority = document.querySelector("#select-priority");
+const SubmitButton = document.querySelector("#submit-button");
+const ClearButton = document.querySelector("#delete-list");
+const listContainer = document.querySelector(".list-container");
+
+
+const renderList=(text, priority)=> {
+    const li = document.createElement("li");
+    const classPrio = ["prio-1", "prio-2", "prio-3"];
+    li.classList.add("todo-li", classPrio[priority-1]);
+    const label = document.createElement("label");
+    label.classList.add("todo-title");
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.classList.add("checkbox-todo");
+    label.append(input);
+    label.append(document.createTextNode(text));
+    li.append(label);
+    listContainer.append(li);
+}
+
+const init = ()=> {
+    for( let task of tasks){
+        renderList(task.text, task.priority);
+    }
+}
+
+init();
+
+// Ajouter une tache
+
+function addTask() {
+    const title = inputTitle.value;
+    const priority = inputPriority.value;
+    const newTask = {
+        text: title,
+        priority: priority,
+    }
+    tasks.push(newTask);
+    listContainer.append(renderList(newTask.text, newTask.priority))
+    console.log(newTask);
+    console.log(tasks);
+}
+
+SubmitButton.addEventListener("click", addTask);
+
+// Supprimer la liste
+
+const DeleteAll = ()=> {
+    listContainer.innerHTML = "";
+    tasks = tasks.splice(0,0)
+;    console.log(tasks);
+    window.alert("Liste supprimée");
+}
+
+ClearButton.addEventListener("click", DeleteAll);
+
+
+// Tri 
+
+const sortTasks = (e) => {
+    e.preventDefault();
+    const sortedTasks = tasks.sort((a,b) => a.priority - b.priority);
+    tasks = sortedTasks;
+    listContainer.innerHTML = "";
+    for (let task of tasks){
+        renderList(task.text, task.priority);
+    }
+}
+
+const SortButtonHight = document.querySelector("#btn-tri-normal");
+
+SortButtonHight.addEventListener("click", sortTasks);
+
+// Tri inverse
+
+const sortTasksReverse = (e) => {
+    e.preventDefault();
+    const sortedTasks = tasks.sort((a,b) => b.priority - a.priority);
+    tasks = sortedTasks;
+    listContainer.innerHTML = "";
+    for (let task of tasks){
+        renderList(task.text, task.priority);
+    }
+}
+
+const SortButtonLow = document.querySelector("#btn-tri-reverse");
+
+SortButtonLow.addEventListener("click", sortTasksReverse);
+
+
+// Supprimer une tache
+const CheckBox = document.querySelectorAll(".checkbox-todo");
+
+
+const NewTasks = (text)=> {
+
+    return tasks.filter((task) => task.text !== text);
+}  
+
+const deleteTask = (e) => {
+    const liParent = e.target.parentElement.parentElement;
+    const labelParent = e.target.parentElement;
+    const text = labelParent.innerText;
+    const newVersion = NewTasks(text);
+    tasks = newVersion;
+    liParent.remove();
+}
+
+
+CheckBox.forEach((checkbox) => {
+    checkbox.addEventListener("change", deleteTask);
+})
+
